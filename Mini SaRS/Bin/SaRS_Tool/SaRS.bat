ad9e7f1c5419c61baea6c2170ff6da8b6100ed4a
Tim4ik <tim4ikgubin@gmail.com>
2022-06-26 17:09:38 +0400

    Delete Mini SaRS/Bin/SaRS_Tool directory

--- Mini SaRS/Bin/SaRS_Tool/SaRS.bat
+++
@@ -1,4 +0,0 @@
-If %1 == SSN Sfc /scannow
-If %1 == DSH Dism /Online /Cleanup-Image /ScanHealth
-If %1 == DRH Dism /Online /Cleanup-Image /RestoreHealth
-REG add HKCU\SOFTWARE\SaRS /v %1 /t REG_SZ /d %ERRORLEVEL%
